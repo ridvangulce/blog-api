91b8d2408f4d8f0b0899eba3c23789b34ca98fac
Rıdvan <ridvangulce@users.noreply.github.com>
2021-02-18 22:16:53 +0300

    second commit

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/home")
+def home():
+    return render_template("index.html")
 
 
-if __name__ == '__main__':
-    app.run()
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
